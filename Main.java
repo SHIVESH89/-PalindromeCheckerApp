81d7cc72a24cc2d3346b1ac70d1676684d6163b8
SHIVESH89 <shivesh2032@gmail.com>
2026-02-25 11:13:39 +0530

    UC7

--- Main.java
+++ Main.java
@@ -1,9 +1,8 @@
 public class Main {
-    String input = "civic";
-    Queue<Character> queue = new LinkedList<>();
-    Stack<Character> stack = new Stack<>();
-for (char c : input.toCharArray()) { queue.add(c); stack.push(c); }
-while (!queue.isEmpty()) {
-        if (queue.poll() != stack.pop()) { isPalindrome = false; break; }
+    String input = "refer";
+    Deque<Character> deque = new ArrayDeque<>();
+for (char c : input.toCharArray()) deque.addLast(c);
+while (deque.size() > 1) {
+        if (deque.removeFirst() != deque.removeLast()) { isPalindrome = false; break; }
     }
 }
