780f7accf5e448ac594b78bd08f8c9937898ee2d
SHIVESH89 <shivesh2032@gmail.com>
2026-02-25 11:11:00 +0530

    UC6

--- Main.java
+++ Main.java
@@ -1,9 +1,9 @@
 public class Main {
-    String input = "noon";
+    String input = "civic";
+    Queue<Character> queue = new LinkedList<>();
     Stack<Character> stack = new Stack<>();
-for (char c : input.toCharArray()) stack.push(c);
-    boolean isPalindrome = true;
-for (char c : input.toCharArray()) {
-        if (c != stack.pop()) { isPalindrome = false; break; }
+for (char c : input.toCharArray()) { queue.add(c); stack.push(c); }
+while (!queue.isEmpty()) {
+        if (queue.poll() != stack.pop()) { isPalindrome = false; break; }
     }
 }
